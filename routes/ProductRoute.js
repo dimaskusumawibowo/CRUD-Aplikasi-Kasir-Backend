c5c09c7a1695ccc0eb8c35bc04b51780350e9f91
dimaskusumawibowo <110800934+dimaskusumawibowo@users.noreply.github.com>
2023-01-18 14:44:54 +0700

    first commit

---
+++ routes/ProductRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    getProducts,
+    getProductsById,
+    createProduct,
+    updateProduct,
+    deleteProduct
+} from "../controller/ProductController.js"
+
+const router = express.Router();
+
+router.get('/products', getProducts);
+router.get('/products/:id', getProductsById);
+router.post('/products',createProduct);
+router.put('/products/:id',updateProduct);
+router.delete('/products/:id',deleteProduct)
+
+
+
+export default router;
